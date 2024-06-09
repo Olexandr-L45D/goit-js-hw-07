30a203d506e97bbafaa1d0f3f596560ddfb50e03
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-09 23:27:56 +0300

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,29 @@
-//6
+//6form.reset()
+const inputNumber = document.querySelector('.js-uze-number');
+inputNumber.addEventListener('input', handlerinput);
+
+function handlerinput(evt) { }
+
+const refs = {
+      body: document.body,
+      btnCreate: document.querySelector('button[data-create = "create"]'),
+      btnDestroy: document.querySelector('button[data-destroy = "destroy"]'),
+  }
+   const INTERVAL_DELAY = 1000;
+  let intervalId = null;
+  refs.btnCreate.addEventListener('click', changeColor);
+  refs.btnDestroy.addEventListener('click', onBtnStop);
+  
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+document.boxes.style.color = getRandomHexColor();  
+const clickbutton = document.querySelector('.boxes');
+clickbutton.addEventListener('click', handlerClick);
+function handlerClick() {}
+function createBoxes(amount) {
+}
+function destroyBoxes() {
+}
