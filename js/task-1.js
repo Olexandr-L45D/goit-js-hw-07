27667e20c475ea9cecbea71a44784987e01da2fa
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-09 18:01:59 +0300

    task-1, 2

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+//
+const categories = document.querySelectorAll('.item').length;
+ console.log(`Number of categories : ${categories}`);
+const items = document.querySelectorAll('.item')
+items.forEach(function(item) {
+    const itemTitle = item.querySelector(h2).textContent;
+    console.log(`Category: ${itemTitle}`);
+    const elementNumber = item.querySelectorAll('ul li').length;
+    console.log(`Elements : ${elementNumber}`);
+})
