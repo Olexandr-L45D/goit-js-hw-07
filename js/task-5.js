ff87db4923e6ba79336e7293f982c3e640bbaf4c
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-09 22:45:03 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -4,15 +4,10 @@ function getRandomHexColor() {
       .toString(16)
       .padStart(6, 0)}`;
   }
-  const clickbutton = document.querySelector(".change-color");
-  clickbutton.addEventListener("click", handlerClick);
-     function handlerClick(evt) {
-  console.log(evt.currentTarget)
-  const span = document.querySelector('span')
-  span.textContent = evt.currentTarget.value;
-  if (evt.currentTarget.value) {
-    document.body.style.backgroundColor
-  }
-  }
-  
-  console.log(getRandomHexColor(clickbutton));
+  const clickbutton = document.querySelector('.change-color');
+const backcolor = document.querySelector('.color');
+clickbutton.addEventListener('click', handlerClick);
+function handlerClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  backcolor.textContent = document.body.style.backgroundColor;
+}
