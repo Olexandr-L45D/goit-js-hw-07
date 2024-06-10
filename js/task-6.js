cfaa681ef8533878e79ac14305468cda7a07dd45
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-10 17:30:25 +0300

    upd task 6

--- js/task-6.js
+++ js/task-6.js
@@ -10,7 +10,7 @@ refs.btnDestroy.addEventListener('click', onDestroyBtnClick);
 
    function onCreateBtnClick(event) {
     const inputValue = refs.inputNumber.value.trim();
-    if  (inputValue >= 1 | inputValue <= 100 ) {
+    if  (inputValue >= 1 || inputValue <= 100 ) {
       alert(`enter the correct value`)
       return inputValue;
    }
@@ -25,6 +25,12 @@ function destroyBoxes() {
   input.value = '';
 }
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 function createBoxes(amount) {
  let sizes = 30;
  const elements = [];
@@ -35,15 +41,10 @@ function createBoxes(amount) {
   sizes += 10;
   newElement.style.backgroundColor = getRandomHexColor();
  }
- //this.elements.push(newElement);
+  this.elements.push(`newElement`);
 }
-this.elements.push(newElement);
+//this.elements.push(newElement);
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
 
 
 
